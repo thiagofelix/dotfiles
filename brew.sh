ac4eab8e3fe8984f172641762e618570a46017a1
AlphaSights <Thiago Felix>
2022-03-03 09:09:57 -0300

    Dotfiles first commit

---
+++ brew.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with macOS are outdated).
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+# brew install homebrew/php/php56 --with-gmp
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install other useful binaries.
+brew install ack
+brew install dark-mode
+brew install git
+brew install git-lfs
+brew install imagemagick --with-webp
+brew install rename
+brew install ssh-copy-id
+brew install tree
+brew install the_silver_searcher
+brew install jenv
+brew install nvm
+brew install tmux
+
+# Install cask applications
+brew install -cask bettertouchtool
+brew install -cask slack
+brew install -cask transmission
+# brew install -cask flux
+# brew install -cask iterm2
+# brew install -cask keeweb
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
