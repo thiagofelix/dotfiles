86d93579ba38501845793a6e5bd8b4e49c41804d
Thiago Felix <me@thiagofelix.com>
2025-01-23 11:13:41 -0300

    disable inlay hints

--- .config/nvim-lazyvim/lua/plugins/lsp.lua
+++ .config/nvim-lazyvim/lua/plugins/lsp.lua
@@ -3,6 +3,9 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = {
+        enabled = false,
+      },
       diagnostics = {
         underline = {
           severity = vim.diagnostic.severity.ERROR,
