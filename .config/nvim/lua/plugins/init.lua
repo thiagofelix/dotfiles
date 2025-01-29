8b08532556ee174049f01b171bb0e25bfa5e7318
Thiago Felix <me@thiagofelix.com>
2025-01-29 10:11:25 -0300

    no swapfile, ignored ai plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   { import = "lazyvim.plugins.extras.lang.sql" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.editor.harpoon2" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.test.core" },
@@ -8,12 +9,19 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "night",
+      on_highlights = function(hl)
+        hl.DiagnosticUnnecessary = { link = "Comment" }
+      end,
     },
   },
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
+    opts = {
+      settings = {
+        expose_as_code_action = { "remove_unused_imports", "fix_all", "add_missing_imports" },
+      },
+    },
   },
   {
     "nvim-neotest/neotest",
@@ -37,6 +45,14 @@ return {
         "<leader>fp",
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
+      },
+
+      {
+        "<leader>sp",
+        function()
+          require("telescope.builtin").live_grep({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Search Plugin File",
       },
     },
     -- change some options
